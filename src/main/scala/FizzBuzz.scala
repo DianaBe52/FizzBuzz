22f07b19954046cff86bec54d2f7dcde2e948c13
Diana Berzina <berzina.diana1@gmail.com>
2021-02-09 20:34:17 +0200

    Added features

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,11 +2,11 @@ object FizzBuzz extends App {
 
   for (i <- 1 to 100) {
     if ( i % 5 == 0 && i % 7 == 0)
-      println(i +" = FizzBuzz")
+      println("FizzBuzz")
     else if (i % 5 == 0)
-      println(i +" = Fizz")
+      println("Fizz")
     else if (i % 7 == 0)
-      println(i +" = Buzz")
+      println("Buzz")
     else
       println(i)
   }
