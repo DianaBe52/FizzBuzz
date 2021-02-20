a916f71a926c9e715e4dd63d7ed88736cc5a934c
Diana Berzina <berzina.diana1@gmail.com>
2021-02-20 20:12:41 +0200

    Temperature calculator

---
+++ src/main/scala/BonusCalculator.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn.readLine
+
+object BonusCalculator extends App {
+  val employee = readLine ("What is your name?")
+  println(s"Nice to talk to you, $employee")
+  val workExperience = readLine ("How many years have you worked for?").toFloat
+  val monthlyWage = readLine ("What is your monthly wage in EUR?").toFloat
+  val christmasBonus = ((workExperience-2) * (monthlyWage*0.15)).toFloat
+  if (workExperience >=2){
+    println(s"Congrats $employee! Your Christmas Bonus is EUR $christmasBonus!")
+  } else{
+    println(s"I'm sorry $employee. You don't have Christmas Bonus this year")
+  }
+
+}
